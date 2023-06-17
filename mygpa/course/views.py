c46fd4db938db9e46c62ff027d8b141d3b52b49e
LmOpe <lawalmuhammed44@gmail.com>
2023-06-17 01:42:10 +0100

    Implement account activation with email feature

--- mygpa/course/views.py
+++ mygpa/course/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
 
 # Create your views here.
+
+@api_view()
+def activateUsersView(request, uid, token):
+    """
+    View to activate users email
+    """
+    post_url = "http://127.0.0.1:8000/auth/users/activation/"
+    post_data = {'uid': uid, 'token': token}
+    result = requests.post(post_url, data=post_data)
+    print(result)
+    content = result.text
+    return Response(content)
--- mygpa/mygpa/settings.py
+++ mygpa/mygpa/settings.py
@@ -135,14 +135,16 @@ REST_FRAMEWORK = {
 
 
 DJOSER = {
-    'SEND_CONFIRMATION_EMAIL': True,
-    'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
-    'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SET_PASSWORD_RETYPE': True,
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'USER_CREATE_PASSWORD_RETYPE': True,
+    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_CONFIRMATION_EMAIL': True,
+    'ACTIVATION_URL': 'auth/users/activate/{uid}/{token}',
+    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
 }
 
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
